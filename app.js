eaf74b52774ae79823ee070e962e600c18a32b26
Robert Skrzypczak <robertskrzypczak@Roberts-MBP.home>
2020-05-15 08:24:44 -0400

    fixes

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-//WORKS//
+//grab store both divs and form inputs in variables
+const titleDisplay = document.getElementById("title-display");
+const weatherDisplay = document.getElementById("weather-display");
+//const miniDisplay = document.getElementById("mini-display");
+//const secondDisplay = document.getElementById('second-display');
+const titleText = document.createElement('h1');
+titleDisplay.append(titleText);
+const weatherTemp = document.createElement('p');
+weatherDisplay.append(weatherTemp);
+const feelsLike = document.createElement('p');
+weatherDisplay.append(feelsLike);
+const humidity = document.createElement('p');
+weatherDisplay.append(humidity);
+const highTemp = document.createElement('p');
+weatherDisplay.append(highTemp);
+const lowTemp = document.createElement('p');
+weatherDisplay.append(lowTemp);
+
 //get all weather data from api call
 const getWeather = async (city, country) => {
   try {
     //http request api
     const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&APPID=5d3ba2952069c3dd1e8cd28c12cd4040`);
     console.log(response.data);
-
-    //grab store both divs and form inputs in variables
-    const weatherDisplay = document.getElementById("weather-display");
-    const miniDisplay = document.getElementById("mini-display");
-    const secondDisplay = document.getElementById('second-display');
     console.log(response.data.name + ",  " + response.data.sys.country);
-    const titleText = document.createElement('h1');
-    weatherDisplay.append(titleText);
+
+    titleDisplay.append(titleText);
     titleText.textContent = response.data.name + "," + response.data.sys.country;
 
     tempMid = response.data.main.temp;
@@ -25,33 +37,22 @@ const getWeather = async (city, country) => {
     backgroundChange()
     //get and display weather temperature//
     console.log("Temp: " + response.data.main.temp);
-    const weatherTemp = document.createElement('p');
-    miniDisplay.append(weatherTemp);
     weatherTemp.textContent = "Temperature " + response.data.main.temp + "°";
-
 
     //get and display feels like temperature//
     console.log("Feels like: " + response.data.main.feels_like);
-    const feelsLike = document.createElement('p');
-    miniDisplay.append(feelsLike);
     feelsLike.textContent = "Feels Like " + response.data.main.feels_like + "°";
 
     //get and display humidity//
     console.log("Humidity: " + response.data.main.humidity);
-    const humidity = document.createElement('p');
-    secondDisplay.append(humidity);
-    humidity.textContent = "Humidity " + response.data.main.humidity;
+    humidity.textContent = "Humidity " + response.data.main.humidity + "%";
 
     //get and display high temperature//
     console.log("high temp: " + response.data.main.temp_max);
-    const highTemp = document.createElement('p');
-    secondDisplay.append(highTemp);
     highTemp.textContent = "High Temperature " + response.data.main.temp_max + "°";
 
     //get and display low temperature//
     console.log("low temp: " + response.data.main.temp_min);
-    const lowTemp = document.createElement('p');
-    secondDisplay.append(lowTemp);
     lowTemp.textContent = "Low Temperature " + response.data.main.temp_min + "°";
 
   } catch (error) {
@@ -59,21 +60,25 @@ const getWeather = async (city, country) => {
   };
 };
 
+//clears all previously search information
+function clearPage() {
+  weatherTemp.textContent = "";
+  feelsLike.textContent = "";
+  humidity.textContent = "";
+  highTemp.textContent = "";
+  lowTemp.textContent = "";
+}
 
-
-//WORKS//
 //processes input and makes http request
 function handleSubmit(e) {
   e.preventDefault();
   var city = document.getElementById("city-input").value;
   var country = document.getElementById("country-input").value;
-  //document.getElementById("GFG").innerHTML = txt;
+  clearPage();
   getWeather(city, country);
-  //console.log(location.city);
-  //console.log(location.country);
 };
 
-//WORKS//
+
 const button = document.querySelector('.btn').addEventListener('click', handleSubmit);//grab button
 
 
