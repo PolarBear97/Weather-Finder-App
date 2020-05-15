d723a4120378fd5ca6c90d8bb6cfb91d9c5a0f92
Robert Skrzypczak <robertskrzypczak@Roberts-MBP.home>
2020-05-15 07:27:15 -0400

    styling and function work

--- app.js
+++ app.js
@@ -1,81 +1,79 @@
-//getElement is a function that grabs the city name property from an endpoint and displays the name to the user
-//function getElement () {
-//  const cityName = document.createElement('h2');
-//  const weatherTemp = document.createElement('current-temp');
-//  const weatherBio = document.createElement('weather-description');
-//  const highTemp = document.createElement('high-temp');
-//  const lowTemp = document.createElement('low-temp');
-//  weatherDisplay.append(cityName);
-//removeCity();
-//}
-//getElement();
+//WORKS//
+//get all weather data from api call
+const getWeather = async (city, country) => {
+  try {
+    //http request api
+    const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&APPID=5d3ba2952069c3dd1e8cd28c12cd4040`);
+    console.log(response.data);
 
-//function removeCity(){
-//  This function should remove the old result when the user searches for a new city.
-//}
-const getWeather = async () => {
-  //try {
-  //http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&units=imperial&APPID=c2ed4a9e3e6c47e000cc300c3eeaafd5
+    //grab store both divs and form inputs in variables
+    const weatherDisplay = document.getElementById("weather-display");
+    const miniDisplay = document.getElementById("mini-display");
+    const secondDisplay = document.getElementById('second-display');
+    console.log(response.data.name + ",  " + response.data.sys.country);
+    const titleText = document.createElement('h1');
+    weatherDisplay.append(titleText);
+    titleText.textContent = response.data.name + "," + response.data.sys.country;
 
-  const response = await axios.get("http://api.openweathermap.org/data/2.5/weather?q=london,uk&units=imperial&APPID=5d3ba2952069c3dd1e8cd28c12cd4040");
-  console.log(response.data);
-  //const cityName = document.createElement('h2');
+    tempMid = response.data.main.temp;
+    function backgroundChange() {
+      if (tempMid > 50) {
+        document.body.style.background = "url('sunset.jpg')";
+        document.body.style.color = "black"
+      }
+    }
+    backgroundChange()
+    //get and display weather temperature//
+    console.log("Temp: " + response.data.main.temp);
+    const weatherTemp = document.createElement('p');
+    miniDisplay.append(weatherTemp);
+    weatherTemp.textContent = "Temperature " + response.data.main.temp + "°";
 
-  //get and display weather temperature//
-  console.log("Temp: " + response.data.main.temp);
-  const weatherTemp = document.createElement('p');
-  const weatherDisplay = document.getElementById("weather-display");
-  weatherDisplay.append(weatherTemp);
-  weatherTemp.textContent = "Temperature " + response.data.main.temp;
 
-  ////get and display feels like temperature//
-  console.log("Feels like: " + response.data.main.feels_like);
-  const feelsLike = document.createElement('p');
-  weatherDisplay.append(feelsLike);
-  feelsLike.textContent = "Feels Like " + response.data.main.feels_like;
+    //get and display feels like temperature//
+    console.log("Feels like: " + response.data.main.feels_like);
+    const feelsLike = document.createElement('p');
+    miniDisplay.append(feelsLike);
+    feelsLike.textContent = "Feels Like " + response.data.main.feels_like + "°";
 
-  ////get and display humidity//
-  console.log("Humidity: " + response.data.main.humidity);
-  const humidity = document.createElement('p');
-  weatherDisplay.append(humidity);
-  humidity.textContent = "Humidity " + response.data.main.humidity;
+    //get and display humidity//
+    console.log("Humidity: " + response.data.main.humidity);
+    const humidity = document.createElement('p');
+    secondDisplay.append(humidity);
+    humidity.textContent = "Humidity " + response.data.main.humidity;
 
-  ////get and display high temperature//
-  console.log("high temp: " + response.data.main.temp_max);
-  const highTemp = document.createElement('p');
-  weatherDisplay.append(highTemp);
-  highTemp.textContent = "High Temperature " + response.data.main.temp_max;
+    //get and display high temperature//
+    console.log("high temp: " + response.data.main.temp_max);
+    const highTemp = document.createElement('p');
+    secondDisplay.append(highTemp);
+    highTemp.textContent = "High Temperature " + response.data.main.temp_max + "°";
 
-  ////get and display low temperature//
-  console.log("low temp: " + response.data.main.temp_min);
-  const lowTemp = document.createElement('p');
-  weatherDisplay.append(lowTemp);
-  lowTemp.textContent = "Low Temperature " + response.data.main.temp_min;
-  //} catch (error) {
-  //console.log(`${error}`)
-  //}
-};
-getWeather();
+    //get and display low temperature//
+    console.log("low temp: " + response.data.main.temp_min);
+    const lowTemp = document.createElement('p');
+    secondDisplay.append(lowTemp);
+    lowTemp.textContent = "Low Temperature " + response.data.main.temp_min + "°";
 
-const getWeatherBio = function () {
-
+  } catch (error) {
+    console.log(`${error}`);
+  };
 };
 
-const getCurrentTemp = function () {
 
+
+//WORKS//
+//processes input and makes http request
+function handleSubmit(e) {
+  e.preventDefault();
+  var city = document.getElementById("city-input").value;
+  var country = document.getElementById("country-input").value;
+  //document.getElementById("GFG").innerHTML = txt;
+  getWeather(city, country);
+  //console.log(location.city);
+  //console.log(location.country);
 };
 
-const getHighTemp = function () {
-
-};
-
-const getLowTemp = function () {
-
-};
-
-const BackGround = function () {
-
-};
-
+//WORKS//
+const button = document.querySelector('.btn').addEventListener('click', handleSubmit);//grab button
 
 
